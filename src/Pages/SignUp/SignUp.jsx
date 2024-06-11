2f77113d42c3e031b12ce38db0178b317d2c282a
Anik66 <anikkumardas966@gmail.com>
2024-06-11 15:34:01 +0600

    4th commit

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,53 +1,21 @@
-import { useContext, useState } from "react";
+
+import { Link } from "react-router-dom";
+
 import { useForm } from "react-hook-form";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { AuthContext } from "../../Providers/AuthProvider";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
 import img1 from "../../assets/register.jpg"
-import toast from "react-hot-toast"
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { createContext, useContext, useState } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
+import toast from "react-hot-toast";
 
 
 const SignUp = () => {
-    const [error,setError] =useState("")
-    const [showPassword,SetShowPassword] =useState(false)
-    const navigate =useNavigate()
-  const{createUser,signInWithGoogle,updateUserProfile,user,setUser} =useContext(AuthContext)
-  
-
-
-  
-  const location =useLocation();
-
-
-  const handleGoogleSignIn = async()=>{
-    try{
-      await signInWithGoogle()
-    toast.success('Sign In successfull')
-    navigate('/')
-    }
-    catch(err){
-      console.log(err)
-      toast.error(err?.message)
-    }
-
-
-  }
-
-
-  
-
-  
-
-  const {
-    register,
-    handleSubmit,
-    
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit =data =>{
-    const {email,password,fullName,photourl} =data
-    
+  const [error,setError] =useState("")
+  const [showPassword, SetShowPassword] = useState(false)
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {createUser} =useContext(AuthContext)
+  const onSubmit = data => {
+    const {email,password,name,photo} =data
     if(password.length <6){
       setError("password length must be 6 character")
       toast.error("password length must be 6 character")
@@ -71,145 +39,176 @@ const SignUp = () => {
 
     createUser(email,password)
     .then(result =>{
-      //create user and updteprofile
-    //   updateUserProfile(fullName,photourl)
-    //   .then(()=>{
-    //     navigate(location?.state ?location.state : "/")
-    //   })
-      //console.log(result)
-      toast.success("Registation Successful",{
-        position:"top-center"
-        
-      });
-      
+      const loggedUser =result.user 
+      console.log(loggedUser)
     })
-    
-  }
-    return (
-      <div className='flex justify-center items-center min-h-[calc(100vh-306px)] my-12'>
-        <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl '>
-          <div className='w-full px-6 py-8 md:px-8 lg:w-1/2'>
-            <div className='flex justify-center mx-auto'>
-            <div className='flex justify-center mx-auto item-center gap-2'>
-              <img className='w-auto h-7  ' lt='' />
-                <span className=' font-bold text-2xl font bold' >HR<span className="text-orange-500">Sync</span> </span>
-             
-           
-            </div>
-            </div>
-  
-            <p className='mt-3 text-xl text-center text-gray-600 '>
-              Get Your Free Account Now.
-            </p>
-  
-            <div onClick={handleGoogleSignIn  }  className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
-              <div className='px-4 py-2'>
-                <svg className='w-6 h-6' viewBox='0 0 40 40'>
-                  <path
-                    d='M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z'
-                    fill='#FFC107'
-                  />
-                  <path
-                    d='M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z'
-                    fill='#FF3D00'
-                  />
-                  <path
-                    d='M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z'
-                    fill='#4CAF50'
-                  />
-                  <path
-                    d='M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z'
-                    fill='#1976D2'
-                  />
-                </svg>
-              </div>
-  
-              <span className='w-5/6 px-4 py-3 font-bold text-center'>
-                Sign in with Google
-              </span>
-            </div>
-  
-            <div className='flex items-center justify-between mt-4'>
-              <span className='w-1/5 border-b  lg:w-1/4'></span>
-  
-              <div className='text-xs text-center text-gray-500 uppercase  hover:underline'>
-                or Registration with email
-              </div>
-  
-              <span className='w-1/5 border-b dark:border-gray-400 lg:w-1/4'></span>
-            </div>
-       
 
-            <form onSubmit={handleSubmit(onSubmit)} className=" animate__animated animate__flipInX card-body">
-    <div className="form-control">
-      <label className="label">
-        <span className="label-text text-2xl font-bold">Name</span>
-      </label>
-      {errors.fullName && <span className=" text-red-500 font-bold pb-1">This field is required</span>}
-      <input type="text" {...register("name", { required: true })} placeholder="name" className="input input-bordered" />
-      
-    </div>
+  };
 
-    <div className="form-control">
-      <label className="label">
-        <span className="label-text text-2xl font-bold">Email</span>
-      </label>
-      {errors.email && <span className=" text-red-500 font-bold pb-1">This field is required</span>}
-      <input type="email" {...register("email", { required: true })} placeholder="email" className="input input-bordered" />
-    </div>
-    <div className="form-control">
-      <label className="label">
-        <span className="label-text text-2xl font-bold">Photo url</span>
-      </label>
-      {errors.photourl && <span className=" text-red-500 font-bold pb-1">This field is required</span>}
-      <input type="text" {...register("photo", { required: true })} placeholder="photourl" className="input input-bordered"  />
-    </div>
-   <div className="relative">
-   <div className="form-control ">
-      <label className="label">
-        <span className="label-text text-2xl font-bold">Password</span>
-      </label>
-      {errors.password && <span className="font-bold pb-1 text-red-500">This field is required</span>}
-      <input type={showPassword?"text":"password"}   {...register("password", { required: true })} placeholder="password" className="input input-bordered" 
-       />
 
-         <span className="absolute  mt-[60px] top-1 right-1" onClick={()=>SetShowPassword(!showPassword)}> {
-                    showPassword? <FaEye></FaEye>:<FaEyeSlash></FaEyeSlash>
-                  }</span>
-       
-     
-    </div>
-   </div>
-    {
+
+
+  return (
+    <div className='flex justify-center items-center min-h-[calc(100vh-306px)] my-12'>
+      <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl '>
+        <div
+          className='hidden bg-cover bg-center lg:block lg:w-1/2'
+          style={{
+            backgroundImage: `url(${img1})`,
+          }}
+        ></div>
+
+        <div className='w-full px-6 py-8 md:px-8 lg:w-1/2'>
+          <div className='flex justify-center mx-auto item-center'>
+            <img className='w-auto h-7  ' alt='' />
+            <span className=' font-bold text-2xl font bold' >HR<span className="text-orange-500">Sync</span> </span>
+
+
+          </div>
+
+          <p className='mt-3 text-xl text-center text-gray-600 '>
+            Welcome back!
+          </p>
+
+          <div className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
+            <div className='px-4 py-2'>
+              <svg className='w-6 h-6' viewBox='0 0 40 40'>
+                <path
+                  d='M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z'
+                  fill='#FFC107'
+                />
+                <path
+                  d='M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z'
+                  fill='#FF3D00'
+                />
+                <path
+                  d='M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z'
+                  fill='#4CAF50'
+                />
+                <path
+                  d='M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z'
+                  fill='#1976D2'
+                />
+              </svg>
+            </div>
+
+            <span className='w-5/6 px-4 py-3 font-bold text-center'>
+              Sign in with Google
+            </span>
+          </div>
+
+          <div className='flex items-center justify-between mt-4'>
+            <span className='w-1/5 border-b  lg:w-1/4'></span>
+
+            <div className='text-xs text-center text-gray-500 uppercase  hover:underline'>
+              or login with email
+            </div>
+
+            <span className='w-1/5 border-b dark:border-gray-400 lg:w-1/4'></span>
+          </div>
+          <form onSubmit={handleSubmit(onSubmit)} >
+
+
+            <div className='mt-4'>
+              <label
+                className='block mb-2 text-sm font-medium text-gray-600 '
+                htmlFor='LoggingEmailAddress'
+              >
+                
+                <span className="label-text text-2xl font-bold">Name</span>
+              </label>
+              <input
+                id='LoggingEmailAddress'
+                autoComplete='email'
+                name='name'{...register("name", { required: true })}
+                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
+                type='text'
+              />
+              {errors.name && <span className="font-bold pb-1 text-red-500">Name is required</span>}
+            </div>
+            <div className='mt-4'>
+              <label
+                className='block mb-2 text-sm font-medium text-gray-600 '
+                htmlFor='LoggingEmailAddress'
+              >
+                
+                <span className="label-text text-2xl font-bold">Photourl</span>
+              </label>
+              <input
+                id='LoggingEmailAddress'
+                autoComplete='email'
+                name='photo'
+                {...register("photo", { required: true })}
+                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
+                type='text'
+              />
+              {errors.photo && <span className="font-bold pb-1 text-red-500">Photourl is required</span>}
+            </div>
+            <div className='mt-4'>
+              <label
+                className='block mb-2 text-sm font-medium text-gray-600 '
+                htmlFor='LoggingEmailAddress'
+              >
+                
+                <span className="label-text text-2xl font-bold">Email Address</span>
+              </label>
+              <input
+                id='LoggingEmailAddress'
+                autoComplete='email'
+                name='email'
+                {...register("email", { required: true })}
+                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
+                type='email'
+              />
+              {errors.email && <span className="font-bold pb-1 text-red-500">Email is required</span>}
+            </div>
+
+            <div className="form-control ">
+              <label className="label">
+                <span className="label-text text-2xl font-bold">Password</span>
+              </label>
+              {errors.password && <span className="font-bold pb-1 text-red-500">password is required</span>}
+              <input type={showPassword ? "text" : "password"}   {...register("password", { required: true })} placeholder="password" className="input input-bordered"
+              />
+
+              <span className="absolute  mt-[60px] top-1 right-1" onClick={() => SetShowPassword(!showPassword)}> {
+                showPassword ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>
+              }</span>
+
+
+            </div>
+            <div className='mt-6'>
+              <button
+                type='submit'
+                className='w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+
+          {
           error && <small className="text-red-600">{error}</small>
           
        }
-    <div className="form-control mt-6">
-      <button className="btn btn-primary">Sign Up</button>
-    </div>
-  </form>
-        
-            <div className='flex items-center justify-between mt-4'>
-              <span className='w-1/5 border-b  md:w-1/4'></span>
-  
-              <Link
-                to='/login'
-                className='text-xs text-gray-500 uppercase  hover:underline'
-              >
-                or sign in
-              </Link>
-  
-              <span className='w-1/5 border-b  md:w-1/4'></span>
-            </div>
+
+          <div className='flex items-center justify-between mt-4'>
+            <span className='w-1/5 border-b  md:w-1/4'></span>
+
+            <Link to='/login' className='text-xs text-gray-500 uppercase  hover:underline'
+            >
+              or sign Ip
+            </Link>
+
+            <Link to="/" className='text-xs text-gray-500 uppercase  hover:underline'> Back To Home</Link>
+
+            <span className='w-1/5 border-b  md:w-1/4'></span>
           </div>
-          <div className='hidden bg-cover bg-center lg:block lg:w-[400px] '
-            style={{
-              backgroundImage: `url(${img1})`,
-            }}
-          ></div>
         </div>
       </div>
-    )
-  }
+
+    </div>
+  );
+};
 
 export default SignUp;
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 
 const Navbar = () => {
+    const {user,logOut } =useContext(AuthContext)
 
     const navOptions = <>
 
@@ -9,7 +12,8 @@ const Navbar = () => {
         <li> <Link to="/pandingwork">PandingWork</Link></li>
         <li> <Link to="/">Dashbroad</Link></li>
         <li> <Link to="/signup">Sign Up</Link></li>
-        <li> <Link to="/login">Login</Link></li>
+        {!user  &&<li><Link to='/login'>Login</Link></li> }
+       
         
         
 
@@ -18,27 +22,52 @@ const Navbar = () => {
 
     </>
     return (
-        <div className="navbar  bg-green-300">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                       {navOptions}
-                    </ul>
-                </div>
-                <Link to="/"><a className="btn btn-ghost text-xl">HR-Sync</a></Link>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {navOptions}
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
-            </div>
+       
+
+
+        <div className='navbar bg-[#50C878] text-white shadow-sm container  px-4 mx-auto'>
+        <div className='flex-1'>
+        <Link to="/" className='flex gap-2 items-center'>
+            <img className='w-auto h-7' alt='' />
+            <span className=' font-bold text-2xl font bold' >Lux<span className="text-orange-500">Stay</span> </span>
+          </Link>
         </div>
+        
+        
+        <div className='flex-none'  role="button" >
+          <ul className='menu menu-horizontal px-1 gap-2 '>
+           {navOptions}
+          </ul>
+  
+          {
+            user &&
+            <div className='dropdown dropdown-end z-50'>
+            <div
+              tabIndex={0}
+              role='button'
+              className='btn btn-ghost btn-circle avatar'
+            >
+              <div title={user?.displayName} className='w-10 rounded-full' >
+                <img
+                  referrerPolicy='no-referrer'
+                  alt='User Profile Photo'
+                  src={user?.photoURL}
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
+            >
+             
+              <li className='mt-2'>
+                <button onClick={logOut} className='bg-gray-100 text-black block text-center '>Logout</button>
+              </li>
+            </ul>
+          </div>
+          }
+        </div>
+      </div>
     );
 };
 
